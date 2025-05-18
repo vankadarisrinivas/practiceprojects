4cb71143c57b6fffe150d84495a4a568ed6b2719
Srinivas <vankadarisrinivas@gmail.com>
2025-05-18 13:21:37 +0530

    Add files via upload

---
+++ PrintNameInTelugu.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class PrintNameInTelugu {
+    public static void main(String[] args) {
+        char ch = '\u0C36';
+        char ch1 = '\u0C40';
+        char ch2 = '\u0C01';
+        char ch3='\u0C28';
+        char ch4='\u0C3F';
+        char ch5='\u0C35';
+        char ch6='\u0C3E';
+        char ch7='\u0C38';
+        char ch8='\u0C4D';
+        System.out.println(""+ch2+ch + ch1+ch3+ch4+ch5+ch6+ch7+ch8);
+
+    }
+}
