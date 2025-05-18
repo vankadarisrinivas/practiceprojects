0b551c2d9e22ef0029375891a89abbad03698ff5
Srinivas <vankadarisrinivas@gmail.com>
2025-05-18 13:29:44 +0530

    Add files via upload

---
+++ SwapValuesWithVariable.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class SwapValuesWithVariable {
+    public static void main(String[] args) {
+
+        int i =10;
+        int j=7;
+        int k= 0;
+        k=i;
+        i=j;
+        j=k;
+
+        System.out.println(i);
+        System.out.println(j);
+
+
+
+    }
+}
